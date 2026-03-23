06fda3198e082216e32820eed6e213e3b4807b30
Voronov Maksim <ravenmax1994@gmail.com>
2026-03-23 17:52:44 +0400

    homework interfaces

---
+++ interfaces/homework_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+type UserService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+
+type MessageService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+
+type Container struct {
+	services map[string]*ServiceDefinition
+}
+
+type ServiceDefinition struct {
+	Name        string
+	Constructor interface{}
+	Instance    interface{}
+}
+
+func NewContainer() *Container {
+	return &Container{
+		services: make(map[string]*ServiceDefinition),
+	}
+}
+
+func (c *Container) RegisterType(name string, constructor interface{}) {
+
+	c.services[name] = &ServiceDefinition{
+		Name:        name,
+		Constructor: constructor,
+	}
+}
+
+func (c *Container) Resolve(name string) (interface{}, error) {
+	// Находим определение сервиса
+	def, exists := c.services[name]
+	if !exists {
+		return nil, fmt.Errorf("service '%s' not found", name)
+	}
+	// Получаем тип конструктора
+	constructorType := reflect.TypeOf(def.Constructor)
+	constructorValue := reflect.ValueOf(def.Constructor)
+
+	// Подготавливаем аргументы для конструктора
+	args := make([]reflect.Value, constructorType.NumIn())
+	for i := 0; i < constructorType.NumIn(); i++ {
+		argType := constructorType.In(i)
+
+		// Пытаемся найти зависимость по имени
+		found := false
+		for _, svc := range c.services {
+
+			instance, err := c.Resolve(svc.Name)
+			if err != nil {
+				return nil, fmt.Errorf("failed to resolve dependency %s: %w", argType.Name(), err)
+			}
+			args[i] = reflect.ValueOf(instance)
+			found = true
+			break
+
+		}
+		if !found {
+			return nil, fmt.Errorf("cannot resolve dependency %s for service %s", argType.Name(), name)
+		}
+	}
+
+	// Вызываем конструктор
+	results := constructorValue.Call(args)
+	instance := results[0].Interface()
+	return instance, nil
+}
+
+func TestDIContainer(t *testing.T) {
+	container := NewContainer()
+	container.RegisterType("UserService", func() interface{} {
+		return &UserService{}
+	})
+	container.RegisterType("MessageService", func() interface{} {
+		return &MessageService{}
+	})
+
+	userService1, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+	userService2, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+
+	u1 := userService1.(*UserService)
+	u2 := userService2.(*UserService)
+	assert.False(t, u1 == u2)
+
+	messageService, err := container.Resolve("MessageService")
+	assert.NoError(t, err)
+	assert.NotNil(t, messageService)
+
+	paymentService, err := container.Resolve("PaymentService")
+	assert.Error(t, err)
+	assert.Nil(t, paymentService)
+}
